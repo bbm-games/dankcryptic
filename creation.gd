5b6eef654407f3afd367cdb5bcab156358dbf65c
newlz <shrimpboyhoftw@gmail.com>
2023-11-13 20:35:24 -0500

    added json parsing now

--- creation.gd
+++ creation.gd
@@ -1,14 +1,47 @@
 extends Node2D
 var tabs
 var hoverSound 
+var lore_data
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	tabs = get_node("TabContainer")
+	
+	# add sound effects to all buttons on hover
 	hoverSound = get_node("hoverSound")
 	connect_buttons(get_tree().root)
 	get_tree().node_added.connect(_on_SceneTree_node_added)
+	
+	# read in JSON and go from there
+	var file = FileAccess.open("lore/lore.json", FileAccess.READ)
+	lore_data = JSON.new().parse_string(file.get_as_text())
 
+	# load in class buttons and base stats
+	var classes = tabs.get_node("Vocation").get_node("HBoxContainer").get_node("GridContainer")
+	for button in classes.get_children():
+		var vocation_name = button.get_text()
+		var vocation_data = get_vocation_stats(button.get_text())
+		# make it so stats UI updates on the button click
+		button.pressed.connect(vocation_button_pressed.bind(vocation_name))
+	
+func vocation_button_pressed(vocation_name):
+	print(vocation_name)
+	# display stat data for the vocation
+	var statsdatajson = get_vocation_stats(vocation_name)
+	var statvalues = tabs.get_node("Vocation/HBoxContainer/VBoxContainer3/HBoxContainer2/VBoxContainer2").get_children()
+	var statlabels = tabs.get_node("Vocation/HBoxContainer/VBoxContainer3/HBoxContainer2/VBoxContainer").get_children()
+	var i = 0
+	for statvalue in statvalues:
+		statvalue.set_text(str(statsdatajson['stats'][statlabels[i].get_text().to_lower()]))
+		i += 1
+	# display lore description too
+	self.get_node("TabContainer/Vocation/HBoxContainer/RichTextLabel").set_text(statsdatajson['lore'])
+func get_vocation_stats(name):
+	for thing in lore_data["vocations"]:
+		if thing["class_name"] == name:
+			return thing
+	return "Couldn't find vocation data for " + name
+	
 func _on_SceneTree_node_added(node):
 	if node is Button:
 		connect_to_button(node)
@@ -41,3 +74,6 @@ func _on_prev_button_pressed():
 func _on_start_game_button_pressed():
 	# start the game
 	get_tree().change_scene_to_file("res://game.tscn")
+
+func _on_char_name_line_edit_text_changed(new_text):
+	get_node("TabContainer/Confirm/VBoxContainer/HBoxContainer/Label").set_text(new_text)
