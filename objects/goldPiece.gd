c5e40145fa92754f274d068995f40ced544b8a4e
newlz <shrimpboyhoftw@gmail.com>
2024-04-07 19:28:16 -0400

    fixed gold piece animation

--- objects/goldPiece.gd
+++ objects/goldPiece.gd
@@ -2,11 +2,13 @@ extends Area2D
 
 var des_vel: Vector2
 var vel = Vector2(0,0)
+var rng 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	rng = RandomNumberGenerator.new()
+	get_node('goldSprite').set_frame(randi_range(0,5))
+	get_node('goldSprite').play()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
